16de40092fe68ec8e076ac146fbbf9ee76eee0c7
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-07 10:36:43 +0530

    react installed for project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Gallery from './components/Gallery/Gallery'
+import Header from './components/Header/Header'
+function App() {
+
+
+  return (
+    <>
+    <div className='app-container'>
+      
+
+    </div>
+    </>
+  )
+}
+
+export default App
